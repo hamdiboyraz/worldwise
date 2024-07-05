0999619c91da1577837076660526afc375c29582
hamdiboyraz <hamdiboyraz19@gmail.com>
2024-07-05 03:17:15 +0300

    Implement cities list and add json-server for data

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,34 @@ import Pricing from './pages/Pricing.jsx';
 import AppLayout from './pages/AppLayout.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
 import Login from './pages/Login.jsx';
+import CityList from './components/CityList.jsx';
+import { useEffect, useState } from 'react';
+
+const BASE_URL = 'http://localhost:8000';
 
 function App() {
-    console.log('App component');
+    const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(function() {
+        async function fethCities() {
+            try {
+                setIsLoading(true);
+                const response = await fetch(`${BASE_URL}/cities`);
+                const data = await response.json();
+                setCities(data);
+                setIsLoading(false);
+            } catch (error) {
+                alert('Error fetching cities', error);
+                setIsLoading(false);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fethCities();
+    }, []);
+
     return (
         <BrowserRouter>
             <Routes>
@@ -17,8 +42,8 @@ function App() {
                 <Route path='/pricing' element={<Pricing />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/app' element={<AppLayout />}>
-                    <Route index element={<p>LIST</p>} />
-                    <Route path='cities' element={<p>List of cities</p>} />
+                    <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+                    <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
                     <Route path='countries' element={<p>List of countries</p>} />
                     <Route path='form' element={<p>Form</p>} />
                 </Route>
